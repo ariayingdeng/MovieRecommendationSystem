9ee8b1dc710d4ab6f894a98838ebc9eb0663d85a
Ying Deng <dengy9@student.douglascollege.ca>
2022-02-06 21:31:59 -0800

    Modified Rater interface

--- src/main/java/com/ying/interfaces/Rater.java
+++ src/main/java/com/ying/interfaces/Rater.java
@@ -3,6 +3,8 @@ package com.ying.interfaces;
 import java.util.ArrayList;
 
 public interface Rater {
+	public String getMyID();
+	public void setMyID(String myID);
 	public void addRating(String item, String rating);
 	public boolean hasRating(String item);
 	public double getRating(String item);
